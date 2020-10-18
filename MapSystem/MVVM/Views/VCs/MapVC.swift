906a311a0f575c159cf5d74b05bd4427204ca3e7
Paper <fredchowdz@gmail.com>
2020-10-18 19:08:00 +0800

    .

--- MapSystem/MVVM/Views/VCs/MapVC.swift
+++ MapSystem/MVVM/Views/VCs/MapVC.swift
@@ -34,9 +34,9 @@ class MapVC: UIViewController {
         
         var anchors: [FloatingPanelState : FloatingPanelLayoutAnchoring] = [
             .full: FloatingPanelLayoutAnchor(absoluteInset: 16, edge: .top, referenceGuide: .safeArea),
-            .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea),
-            .tip: FloatingPanelLayoutAnchor(absoluteInset: 44, edge: .bottom, referenceGuide: .safeArea),
-            .hidden: FloatingPanelLayoutAnchor(fractionalInset: 0.1, edge: .bottom, referenceGuide: .safeArea),
+            .half: FloatingPanelLayoutAnchor(fractionalInset: 0.7, edge: .bottom, referenceGuide: .safeArea),
+            .tip: FloatingPanelLayoutAnchor(fractionalInset: 0.3, edge: .bottom, referenceGuide: .safeArea),
+            .hidden: FloatingPanelLayoutAnchor(absoluteInset: 44, edge: .bottom, referenceGuide: .safeArea),
         ]
     }
     internal lazy var panel: FloatingPanelController = {
